3fd0217e61aac92d7189b92fcb8a769cff2b6e22
Hà Vũ Sơn Tùng <Havusontung007@gmail.com>
2021-03-21 03:34:24 +0700

    MasterAuthentication xD

--- Student Unit Test/util/Manage_TestClass.cs
+++ Student Unit Test/util/Manage_TestClass.cs
@@ -20,11 +20,11 @@ namespace Student_Unit_Test.util
             IManager manager = new Manager();
 
 
-                Assert.AreEqual(true, manager.AddCourse(new Course(courseId, courseName)));
+            Assert.AreEqual(true, manager.AddCourse(new Course(courseId, courseName)));
 
-                var ex = Assert.Throws<ContainKeyException>(() => manager.AddCourse(new Course(courseId, courseName)));
-                Assert.That(ex.Message, Is.EqualTo("Id has been existed!!"));
-            
+            var ex = Assert.Throws<ContainKeyException>(() => manager.AddCourse(new Course(courseId, courseName)));
+            Assert.That(ex.Message, Is.EqualTo("Id has been existed!!"));
+
         }
 
         [TestCase("1", "Discrete mathematic")]
@@ -66,38 +66,65 @@ namespace Student_Unit_Test.util
 
         }
 
-
         [TestCase("1", "1", "blah")]
         [TestCase("", "", "")]
-        public void TC05_AddStudentCourse(string courseId, string studentId, string term)
+        public void TC05_AddStudentCourse_Success(string courseId, string studentId, string term)
         {
             IManager manager = new Manager();
-            try
-            {
-                Assert.AreEqual(true, manager.AddStudentCourse(new StudentCourse(courseId, studentId, term)));
-            }
-            catch (ContainKeyException ex)
-            {
-                Assert.AreEqual(ex.Message, "Id has been existed!!");
-            }
-            catch (StudentCourseException ex)
-            {
-                Assert.AreEqual(ex.Message, "Cannot add this studentCourse!!!");
-            }
+            manager.AddStudent(new Student(studentId, "", "", 0, ""));
+            manager.AddCourse(new Course(courseId, ""));
+            Assert.AreEqual(true, manager.AddStudentCourse(new StudentCourse(courseId, studentId, term)));
+        }
 
+
+
+        [TestCase("1", "1", "blah")]
+        public void TC05_AddStudentCourse_ContainKeyException(string courseId, string studentId, string term)
+        {
+            IManager manager = new Manager();
+            manager.AddStudent(new Student(studentId, "", "", 0, ""));
+            manager.AddCourse(new Course(courseId, ""));
+            manager.AddStudentCourse(new StudentCourse(courseId, studentId, term));
+            var ex = Assert.Throws<ContainKeyException>(() => manager.AddStudentCourse(new StudentCourse(courseId, studentId, term)));
+            Assert.AreEqual(ex.Message, "Id has been existed!!");
+        }
+
+
+
+
+        [TestCase("1", "1", "blah")]
+        public void TC06_AddStudentCourse_MissingStudent_StudentCourseException(string courseId, string studentId, string term)
+        {
+            IManager manager = new Manager();
+            manager.AddCourse(new Course(courseId, ""));
+            var ex = Assert.Throws<StudentCourseException>(() => manager.AddStudentCourse(new StudentCourse(courseId, studentId, term)));
+            Assert.AreEqual(ex.Message, "Cannot add this studentCourse!!!");
+        }
+
+
+
+        [TestCase("1", "1", "blah")]
+        public void TC07_AddStudentCourse_MissingCourse_StudentCourseException(string courseId, string studentId, string term)
+        {
+            IManager manager = new Manager();
+            manager.AddStudent(new Student(studentId, "", "", 0, ""));
+            var ex = Assert.Throws<StudentCourseException>(() => manager.AddStudentCourse(new StudentCourse(courseId, studentId, term)));
+            Assert.AreEqual(ex.Message, "Cannot add this studentCourse!!!");
         }
 
 
         [TestCase("1")]
         [TestCase("")]
-        public void TC04_DeleteCourse(string courseId)
+        public void TC08_DeleteCourse(string courseId)
         {
             IManager manager = new Manager();
             manager.AddCourse(new Course(courseId, ""));
             Assert.IsTrue(manager.DeleteCourse(courseId));
         }
+
+
         [TestCase("1")]
-        public void TC05_DeleteCourse_ReturnFalse(string courseId)
+        public void TC09_DeleteCourse_ReturnFalse(string courseId)
         {
             IManager manager = new Manager();
 
@@ -108,7 +135,7 @@ namespace Student_Unit_Test.util
 
         [TestCase("1")]
         [TestCase("")]
-        public void TC06_DeleteStudent(string studentId)
+        public void TC10_DeleteStudent(string studentId)
         {
             IManager manager = new Manager();
             manager.AddStudent(new Student(studentId, "", "", 0, ""));
@@ -116,30 +143,33 @@ namespace Student_Unit_Test.util
         }
 
 
+
         [TestCase("1")]
         [TestCase("")]
-        public void TC07_DeleteStudent_ReturnFalse(string studentId)
+        public void TC11_DeleteStudent_ReturnFalse(string studentId)
         {
             IManager manager = new Manager();
             Assert.IsFalse(manager.DeleteStudent(studentId));
         }
 
 
+
         [TestCase("1", "1", "blah")]
         [TestCase("", "", "")]
-        public void TC08_DeleteStudentCourse(string courseId, string studentId, string term)
+        public void TC12_DeleteStudentCourse(string courseId, string studentId, string term)
         {
             IManager manager = new Manager();
-            manager.AddStudent(new Student(studentId,"", "", 0, ""));
-            manager.AddCourse(new Course(courseId,""));
+            manager.AddStudent(new Student(studentId, "", "", 0, ""));
+            manager.AddCourse(new Course(courseId, ""));
             manager.AddStudentCourse(new StudentCourse(studentId, courseId, term));
             Assert.IsTrue(manager.DeleteStudentCourse(new StudentCourse(courseId, studentId, term)));
 
         }
 
+
         [TestCase("1", "1", "blah")]
         [TestCase("", "", "")]
-        public void TC09_DeleteStudentCourse_ReturnFalse(string courseId, string studentId, string term)
+        public void TC13_DeleteStudentCourse_ReturnFalse(string courseId, string studentId, string term)
         {
             IManager manager = new Manager();
             Assert.IsFalse(manager.DeleteStudentCourse(new StudentCourse(courseId, studentId, term)));
@@ -148,71 +178,77 @@ namespace Student_Unit_Test.util
 
         [TestCase("1", "Tung", "Ha Vu Son", 21, "havusontung007@gmail.com")]
         [TestCase("", "", "", 0, "")]
-        public void TC10_EditStudent(string studentId, string studentFirstName, string studentLastName, int studentAge, string email)
+        public void TC14_EditStudent_Success(string studentId, string studentFirstName, string studentLastName, int studentAge, string email)
         {
             IManager manager = new Manager();
-            try
-            {
-                manager.AddStudent(new Student(studentId, "", "", studentAge, ""));
-                Assert.IsTrue(manager.EditStudent(new Student(studentId, studentFirstName, studentLastName, studentAge, email)));
-            }
-            catch(CannotUpdateCourseException ex)
-            {
-                Assert.AreEqual(ex.Message, "Cannot Update Student " + studentLastName);
-            }
+            manager.AddStudent(new Student(studentId, "", "", studentAge, ""));
+            Assert.IsTrue(manager.EditStudent(new Student(studentId, studentFirstName, studentLastName, studentAge, email)));
+
+        }
+
+
+        [TestCase("1", "Tung", "Ha Vu Son", 21, "havusontung007@gmail.com")]
+        [TestCase("", "", "", 0, "")]
+        public void TC15_EditStudent_Failure(string studentId, string studentFirstName, string studentLastName, int studentAge, string email)
+        {
+            IManager manager = new Manager();
+            manager.AddStudent(new Student("1231321", "", "", studentAge, ""));
+            var ex = Assert.Throws<CannotUpdateStudentException>(() => manager.EditStudent(new Student(studentId, studentFirstName, studentLastName, studentAge, email)));
+            Assert.AreEqual(ex.Message, "Cannot Update Student " + studentLastName);
         }
 
         [TestCase("1", "1")]
         [TestCase("", "")]
-        public void TC11_EditCourse(string courseId, string courseName)
+        public void TC16_EditCourse_Success(string courseId, string courseName)
         {
             IManager manager = new Manager();
-            try
-            {
-                manager.AddCourse(new Course(courseId, courseName));
-                Assert.IsTrue(manager.EditCourse(new Course(courseId, courseName)));
-            }
-            catch (CannotUpdateCourseException ex)
-            {
-                Assert.AreEqual(ex.Message, "Cannot Update Course " + courseName);
-            }
+            manager.AddCourse(new Course(courseId, courseName));
+            Assert.IsTrue(manager.EditCourse(new Course(courseId, courseName)));
+
         }
 
 
-        [TestCase("1", "1","")]
-        [TestCase("", "","")]
-        public void TC12_EditStudentCourse(string courseId, string studentId, string term)
+        [TestCase("1", "1")]
+        [TestCase("", "")]
+        public void TC17_EditCourse_Failure(string courseId, string courseName)
         {
             IManager manager = new Manager();
-            try
-            {
-                manager.AddStudent(new Student(studentId, "", "", 0, ""));
-                manager.AddCourse(new Course(courseId, ""));
-                manager.AddStudentCourse(new StudentCourse(studentId, courseId, ""));
-                Assert.IsTrue(manager.EditStudentCourse(new StudentCourse(courseId, studentId,term)));
-            }
-            catch (CannotUpdateStudentCourseException ex)
-            {
-                Assert.AreEqual(ex.Message, "Cannot Update this StudentCourse ");
-            }
+            manager.AddCourse(new Course("321321321", courseName));
+            var ex = Assert.Throws<CannotUpdateCourseException>(() => manager.EditCourse(new Course(courseId, courseName)));
+            Assert.AreEqual(ex.Message, "Cannot Update Course " + courseName);
+
+        }
+
+
+
+        [TestCase("1", "1", "")]
+        [TestCase("", "", "")]
+        public void TC18_EditStudentCourse_Success(string courseId, string studentId, string term)
+        {
+            IManager manager = new Manager();
+            manager.AddStudent(new Student(studentId, "", "", 0, ""));
+            manager.AddCourse(new Course(courseId, ""));
+            manager.AddStudentCourse(new StudentCourse(studentId, courseId, ""));
+            Assert.IsTrue(manager.EditStudentCourse(new StudentCourse(courseId, studentId, term)));
+
         }
 
         [TestCase]
-        public void TC13_GetStudentReport()
+        public void TC19_GetStudentReport()
         {
-            Student student = new Student("HE141010","Tung","Ha Vu Son",21,"havusontung007@gmail.com");
+            Student student = new Student("HE141010", "Tung", "Ha Vu Son", 21, "havusontung007@gmail.com");
             IManager manager = new Manager();
             manager.AddStudent(student);
             string output = student.ToString();
             string actual = String.IsNullOrEmpty(output) ? "\nStudent List is Empty!!\n" : "\n" + output + "\n";
             Assert.AreEqual(actual, manager.GetStudentReport());
-           
+
         }
 
         [TestCase]
-        public void TC14_GetCourseReport()
+        public void TC20_GetCourseReport()
         {
-            Course course = new Course("1","Discrete Mathematics");
+            Course course = new Course("1", "Discrete Mathematics");
             IManager manager = new Manager();
             manager.AddCourse(course);
             string output = course.ToString();
@@ -223,11 +259,11 @@ namespace Student_Unit_Test.util
 
 
         [TestCase]
-        public void TC15_GetStudentCourseReport()
+        public void TC21_GetStudentCourseReport()
         {
             Student student = new Student("HE141010", "Tung", "Ha Vu Son", 21, "havusontung007@gmail.com");
             Course course = new Course("1", "Discrete Mathematics");
-            StudentCourse studentCourse = new StudentCourse("1","HE141010","termWhat?");
+            StudentCourse studentCourse = new StudentCourse("1", "HE141010", "termWhat?");
             IManager manager = new Manager();
             manager.AddCourse(course);
             manager.AddStudent(student);
